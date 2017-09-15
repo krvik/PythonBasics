21104013fcf1b67655548e291aa89d593598f156
Vikash Kumar <vikashkumardev1996@gmail.com>
2017-09-15 20:05:28 +0530

    Basics of turtle graaphics library

---
+++ LICENSE.md/94turtle_rainbowBenzene.py
@@ -0,0 +1,18 @@
+# Python program to draw
+# Rainbow Benzene
+# Author :github.com/vikash1996
+
+import turtle
+import time
+
+colors = ['red', 'purple', 'blue', 'green', 'orange', 'yellow']
+t = turtle.Pen()
+turtle.bgcolor('black')
+for x in range(360):
+    t.pencolor(colors[x%6])
+    t.width(x/100 + 1)
+    t.forward(x)
+    t.left(59)
+
+time.sleep(10)
+turtle.bye()
---
+++ LICENSE.md/9turtle.py
@@ -0,0 +1,16 @@
+# this is 9th python programme.
+# Created by me.
+# python3
+
+import turtle               # allows us to use the turtles library
+turtle.forward(80)
+turtle.right(90)
+for i in range(20):
+    turtle.forward(10)
+    turtle.right(10)
+
+turtle.mainloop()
+
+
+
+
